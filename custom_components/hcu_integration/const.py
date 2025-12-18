ed6007ab6e3aae94e4f7bbb578a97953c1f1efeb
Phil7989 <erosiv0g@icloud.com>
2025-12-18 17:24:09 +0100

    resolve angle icon

--- custom_components/hcu_integration/const.py
+++ custom_components/hcu_integration/const.py
@@ -546,7 +546,7 @@ HMIP_FEATURE_TO_ENTITY = {
     "absoluteAngle": {
         "class": "HcuGenericSensor",
         "name": "Absolute Angle",
-        "icon": "mdi:angle-acuteangle-acute",
+        "icon": "mdi:angle-acute",
         "state_class": SensorStateClass.MEASUREMENT,
         "entity_registry_enabled_default": False,
     },

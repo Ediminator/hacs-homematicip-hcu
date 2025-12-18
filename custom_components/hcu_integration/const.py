b8b1d7fbb75f77bf7bbcb73c1c0107bc3d6ed62f
Phil7989 <erosiv0g@icloud.com>
2025-12-18 15:19:19 +0100

    Angle disable by default

--- custom_components/hcu_integration/const.py
+++ custom_components/hcu_integration/const.py
@@ -547,6 +547,7 @@ HMIP_FEATURE_TO_ENTITY = {
         "class": "HcuGenericSensor",
         "name": "Absolute Angle",
         "icon": "mdi:angle-acuteangle-acute",
+        "entity_registry_enabled_default": False,
     },
     "mainsVoltage": {
         "class": "HcuGenericSensor",
